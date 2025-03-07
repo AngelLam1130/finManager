294cc7783438b318c2d436ad1bfe675bb8de17f2
AngelLam1130 <anqi_lin1130@163.com>
2025-03-07 12:31:25 -0800

    Initial Commit

---
+++ finManager/finManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  finManagerApp.swift
+//  finManager
+//
+//  Created by Angel Lam on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct finManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ finManagerTests/finManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  finManagerTests.swift
+//  finManagerTests
+//
+//  Created by Angel Lam on 3/7/25.
+//
+
+import Testing
+
+struct finManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
